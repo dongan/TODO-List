6dbfc984ec5ade1e7070aa1edb9e51aa095472a7
dongan <don1988gan@mail.ru>
2014-05-01 13:13:10 +0400

    1

---
+++ MySiteB/Models/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace ToDoListD.Models
+{
+    public class TasksDBContext : DbContext
+    {
+        public TasksDBContext() : base("TaskDB") { }
+        public DbSet<task> tasks{get; set;}
+       
+    }
+}
