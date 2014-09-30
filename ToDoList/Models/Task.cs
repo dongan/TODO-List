7949026bab0bfa8e1ec76f89cbadaeaef1374572
dongan <don1988gan@mail.ru>
2014-09-30 13:00:35 +0400

    1

---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        public int id { get; set; }
+        public int priority { get; set; }
+        public string task { get; set; }
+    }
+}
